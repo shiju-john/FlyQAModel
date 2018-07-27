e8009bc2f8b5521c72da3491cf3f5a8e5f64259e
unknown <Ashik.Veliyanoor@FLY-DT244.flytxt.com>
2018-07-27 16:21:09 +0530

    table changes

--- chat-bot-ui-Graph/src/App.vue
+++ chat-bot-ui-Graph/src/App.vue
@@ -74,7 +74,7 @@
       return {
         remarks: [],
         question: String,
-        token:'1fccd19d207874326a0be51efaea17d5a54a88bdc9e6d36b7bbb757092906d7b8a1693cc9eca8b',
+        token:'',
         version: String,
         flag: 'false',
         cardtitle: '',
